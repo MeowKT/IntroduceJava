0e9a52b3785590aea9795165d82334f7131fb066
Savva <wot-sb@yandex.ru>
2019-11-26 00:28:06 +0300

    nmk game fixed

--- nmkgame/src/nmk/Main.java
+++ nmkgame/src/nmk/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        final Game game = new Game(false, List.of(new HumanPlayer(), new RandomPlayer()));
+        final Game game = new Game(false, new HumanPlayer(), new RandomPlayer());
 
         game.start();
     }
